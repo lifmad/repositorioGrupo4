4885b7d02cedf6c0c6279a9e9cebe26a8c00afb7
Leon <anthonygarayleon@gmail.com>
2022-01-25 20:19:59 -0300

    recorrido de los integrantes

--- a.js
+++ a.js
@@ -1,1 +1,4 @@
 let integrantes = ["Beimar", "Anthony", "Ulises"]
+
+integrantes.forEach((integrante,posicion) => console.log(`El integrante ${posicion+1} que conforman el grupo 4 es: ${integrante}`));
+
